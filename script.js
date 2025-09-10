6580290657d52065ead038abb37e792a04f16909
Mayck Eduardo <mayck_22.03@hotmail.com>
2025-09-10 11:15:09 -0300

    feat: - pares de cartas em matriz; - constante para 6 imagens; - verificação de cartas iguais

--- script.js
+++ script.js
@@ -1,3 +1,25 @@
+jogoAberto = true;
+
+const matriz = [];
+const contagem = {};
+
+// Inicializa a contagem de repetições para cada número de 1 a 6
+for (let i = 1; i <= 6; i++) {
+    contagem[i] = 0;
+}
+
+for (let i = 1; i <= 12; i++) {
+    let numeroAleatorio;
+    do {
+        numeroAleatorio = Math.floor(Math.random() * 6) + 1;
+    } while (contagem[numeroAleatorio] >= 2); // Repete se o número já tem 2 repetições
+
+    contagem[numeroAleatorio]++;
+    matriz.push([numeroAleatorio]);
+}
+
+
+
 document.addEventListener("DOMContentLoaded", function () {
     //criando as 12 cartas com divs
     for (let i = 1; i <= 12; i++) {
@@ -9,7 +31,15 @@ document.addEventListener("DOMContentLoaded", function () {
     }
 
 
-
+    //array 6 imagens 12 cartas, ex. carta1 = img3 essa informção está em paresCartas agora vamos armazenar o caminho das 6 imagens
+    const imagens = {
+        1: "imagens/img1.png",
+        2: "imagens/img2.png",
+        3: "imagens/img3.png",
+        4: "imagens/img4.png",
+        5: "imagens/img5.png",
+        6: "imagens/img6.png"
+    };
 
     for (let i = 0; i < carta.length; i++) {
         carta[i].addEventListener("click", function () {
@@ -17,14 +47,15 @@ document.addEventListener("DOMContentLoaded", function () {
             if (document.querySelectorAll(".virar").length < 2) {
                 carta[i].classList.toggle("virar");
                 console.log("Clicou na carta " + (i + 1));
+
                 carta[i].style.backgroundColor = "lightgray";
-                carta[i].style.backgroundImage = "url('imagens/" + (i + 1) + ".png')";
+                carta[i].style.backgroundImage = "url('" + imagens[matriz[i][0]] + "')";
 
             }
             //logica para verificar cartas iguais
             if (document.querySelectorAll(".virar").length === 2) {
                 let cartasViradas = document.querySelectorAll(".virar");
-                if (cartasViradas[0].id === cartasViradas[1].id) {
+                if (cartasViradas[0].style.backgroundImage === cartasViradas[1].style.backgroundImage) {
                     console.log("Encontrou um par!");
                 } else {
                     console.log("Não é um par.");
